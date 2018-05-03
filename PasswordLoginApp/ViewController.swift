2e81d2632172f46759d336a95987bea6bb6fbf36
Sinisa Vukovic <sinisa503@gmail.com>
2018-05-03 20:17:26 +0200

    Initial Commit

---
+++ PasswordLoginApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PasswordLoginApp
+//
+//  Created by Sinisa Vukovic on 03/05/2018.
+//  Copyright © 2018 Sinisa Vukovic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+   override func viewDidLoad() {
+      super.viewDidLoad()
+      // Do any additional setup after loading the view, typically from a nib.
+   }
+
+   override func didReceiveMemoryWarning() {
+      super.didReceiveMemoryWarning()
+      // Dispose of any resources that can be recreated.
+   }
+
+
+}
+
---
+++ PasswordLoginAppTests/PasswordLoginAppTests.swift
@@ -0,0 +1,36 @@
+//
+//  PasswordLoginAppTests.swift
+//  PasswordLoginAppTests
+//
+//  Created by Sinisa Vukovic on 03/05/2018.
+//  Copyright © 2018 Sinisa Vukovic. All rights reserved.
+//
+
+import XCTest
+@testable import PasswordLoginApp
+
+class PasswordLoginAppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
